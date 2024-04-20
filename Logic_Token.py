7e3d1bd3bf644b3d265f169fd244bbdc9bcc336f
Kharu217 <artwins007@gmail.com>
2024-04-20 20:35:38 +0900

    커밋

---
+++ Logic_Token.py
@@ -0,0 +1,30 @@
+from enum import Enum, auto
+from typing import Optional
+
+class Tokens_type(Enum) :
+    AND = auto()
+    OR = auto()
+    XOR = auto()
+    NAND = auto()
+    NOR = auto()
+    NOT = auto()
+    
+    IDENTIFIER = auto()
+    EQUAL = auto()
+    
+    NUMBER = auto()
+    
+    EOF = auto()
+    
+    @classmethod
+    def has_value(cls, value) :
+        return any(value == item.value for item in cls)
+
+class Token:
+    def __init__(self, token_type: Tokens_type, lexeme: str):
+        self.type = token_type
+        self.lexeme = lexeme
+
+    def __str__(self):
+        return f'{self.type.name} {self.lexeme}'
+        
---
+++ Logic_lang.py
@@ -0,0 +1,46 @@
+from Logic_Token import Token, Tokens_type
+from typing import Optional, List
+
+#코드를 토큰 단위로 스캔
+class Logic_Lexer :
+    def __init__(self, source : str) -> None:
+        self.source = source    # 소스 코드
+        self.tokens = []        # 파싱된 토큰
+        self.start = 0			# 토큰의 시작
+        self.current = 0		# 토큰의 끝
+        self.line = 1			# 라인
+    
+    def lex(self) -> List[Token]:
+        while not self.is_end():
+            self.start = self.current
+            self.scan_token()
+
+        self.tokens.append(Token(Tokens_type.EOF, ''))
+
+        return self.tokens
+    
+    def scan_tokens(self) :
+        ch = self.advance()
+        
+        if ch == 'AND' :
+            self.add_token(Tokens_type.AND)
+        elif ch == 'OR' :
+            self.add_token(Tokens_type.OR)
+        elif ch == 'XOR' :
+            self.add_token(Tokens_type.XOR)
+        elif ch == 'NAND' :
+            self.add_token(Tokens_type.NAND)
+        elif ch == 'NOR' :
+            self.add_token(Tokens_type.NOR)
+        elif ch == 'NOT' :
+            self.add_token(Tokens_type.NOT)
+        
+    def add_token(self) :
+        self.tokens.append(Token(Tokens_type, self.source[self.start: self.current]))
+    
+    def is_end(self) -> bool:
+        return self.current >= len(self.source)
+    
+    def advance(self) -> str :
+        self.current += 1
+        return self.source[self.current - 1]

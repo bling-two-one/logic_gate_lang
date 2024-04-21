97e87d4b8b6a365981a0979cc4e989f36985ab73
Kharu217 <artwins007@gmail.com>
2024-04-21 18:17:36 +0900

    테스트 파일 수정

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 import Logic_lang
 
-s = Logic_lang.token_collect(['AND','OR','XOR','NAND','NOR','NOT','1','101001','>>2','<<4','test=1010','=test2'])
+s = Logic_lang.token_collect(['A=101001',
+                              'B=110101',
+                              '101010',
+                              'AND',
+                              '10100'])
 while not s.is_end() :
     s.scan()
     
